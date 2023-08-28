9b9976e2bd3b20e6203ff332bebd0de39b67a3ee
José Carlos Bran <117953364+Jbranadev@users.noreply.github.com>
2023-08-28 12:59:08 -0600

    Se probo la librería y se añadio las dependencias para el tratamiento correcto de los tipos a enviar

--- src/test/java/io/github/josecarlosbran/JBRestAPI/JBRestAPITest.java
+++ src/test/java/io/github/josecarlosbran/JBRestAPI/JBRestAPITest.java
@@ -1,11 +1,16 @@
 package io.github.josecarlosbran.JBRestAPI;
 
+import com.google.gson.JsonObject;
 import com.josebran.LogsJB.LogsJB;
+import com.josebran.LogsJB.Numeracion.NivelLog;
 import io.github.josecarlosbran.JBRestAPI.Enumeraciones.contentType;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -18,13 +23,9 @@ public class JBRestAPITest {
     @Test(testName = "Test Jakarta Client")
     public void testJakartaClient() throws ValorUndefined {
         LogsJB.info("Inicio Test: ");
-        Client client = ClientBuilder.newClient();
-        /*WebTarget myResource = client.target("https://pokeapi.co/api/v2/pokemon/ditto");
-        String response = myResource.request(MediaType.TEXT_PLAIN)
-                .get(String.class);*/
-        JBRestAPI clienteJB=JBRestAPI.builder().Url("https://pokeapi.co/api/v2/pokemon/ditto").MediaType(MediaType.TEXT_PLAIN_TYPE).newClient();
+        JBRestAPI clienteJB=JBRestAPI.builder().Url("http://localhost:8081/WebServicesPrueba/hello").MediaType(MediaType.APPLICATION_JSON_TYPE).newClient();
         int i=0;
-        while(i<100){
+        while(i<1000){
             String response = clienteJB.request.get(String.class);
             LogsJB.info("Result is: " + response);
             i++;
@@ -38,11 +39,66 @@ public class JBRestAPITest {
         RestApi work=new RestApi();
         work.setContenttype(contentType.JSON);
         int i=0;
-        while(i<100){
-            String response = work.Get("https://pokeapi.co/api/v2/pokemon/ditto", "");;
+        while(i<1000){
+            String response = work.Get("http://localhost:8081/WebServicesPrueba/hello", "");;
             LogsJB.info("Result is: " + response);
             i++;
         }
     }
+
+    @Test(testName = "Test Jakarta Client Post")
+    public void testJakartaClientPost() throws ValorUndefined {
+        LogsJB.info("Inicio Test: ");
+        Log log=new Log();
+        log.setNivelLog(NivelLog.TRACE.name());
+        log.setMetodo("testJakartaClientPost");
+        log.setFecha("2023/08/24 15:43:55 422");
+        log.setClase("io.github.josecarlosbran.LogsJB.LogsJBTest");
+        JBRestAPI clienteJB=JBRestAPI.builder().Url("http://localhost:8081/WebServicesPrueba/Logs").MediaType(MediaType.APPLICATION_JSON_TYPE).newClient();
+        int i=0;
+        while(i<10000){
+            try{
+                log.setTexto("Comentario # "+i);
+                Response respuesta= clienteJB.post(Entity.entity(log, MediaType.APPLICATION_JSON_TYPE));
+                LogsJB.info("Result is: " + respuesta);
+                i++;
+            }catch (Exception e){
+                com.josebran.LogsJB.LogsJB.fatal("Excepción capturada en el metodo encargado de crear " +
+                        "la tabla de Log en BD's");
+                com.josebran.LogsJB.LogsJB.fatal("Tipo de Excepción : " + e.getClass());
+                com.josebran.LogsJB.LogsJB.fatal("Causa de la Excepción : " + e.getCause());
+                com.josebran.LogsJB.LogsJB.fatal("Mensaje de la Excepción : " + e.getMessage());
+                com.josebran.LogsJB.LogsJB.fatal("Trace de la Excepción : " + ExceptionUtils.getStackTrace(e));
+            }
+        }
+    }
+
+    @Test(testName = "Test RestAPI Bran Post",
+            dependsOnMethods = "testJakartaClientPost")
+    public void testWorkAPIPost() throws ValorUndefined {
+        LogsJB.info("Inicio Test: ");
+        Log log=new Log();
+        log.setNivelLog(NivelLog.TRACE.name());
+        log.setMetodo("Test RestAPI Bran Post");
+        log.setFecha("2023/08/24 15:43:55 422");
+        log.setClase("io.github.josecarlosbran.LogsJB.LogsJBTest");
+        JsonObject json=new JsonObject();
+        json.addProperty("nivelLog", log.getNivelLog());
+
+        json.addProperty("clase", log.getClase());
+        json.addProperty("metodo", log.getMetodo());
+        json.addProperty("fecha", log.getFecha());
+        RestApi work=new RestApi();
+        work.setContenttype(contentType.JSON);
+        int i=0;
+        while(i<10000){
+            log.setTexto("Comentario # "+i);
+            json.addProperty("texto", log.getTexto());
+            String response = work.Post("http://localhost:8081/WebServicesPrueba/Logs", json.toString(), "");;
+            LogsJB.info("Result is: " + response);
+            i++;
+        }
+    }
+
 
 }
